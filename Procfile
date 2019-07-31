578618ceb89b57b2143560078bbf48d370f702cc
wodlf3393 <wodlf3393@naver,com>
2019-07-31 16:35:26 +0900

    second commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn blogPorject.wsgi --log-file -
--- blogPorject/settings.py
+++ blogPorject/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,3 +131,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')      # static 폴더를 모아줄
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')   # media 폴더 등록
 MEDIA_URL = '/media/'   # media 폴더로 갈 경로 url 설정
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
